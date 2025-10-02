3f56eb7732e4088226eef7c342bd61aea4c4a611
chukwuisrael146-gif <chukwuisrael146@gmail.com>
2025-10-02 09:52:06 -0700

    My first commit

--- validation.js
+++ validation.js
@@ -4,6 +4,21 @@ const email_input = document.getElementById('email_input');
 const password_input = document.getElementById('password_input');
 const confirm_password_input = document.getElementById('confirm_password_input');
 const error_message = document.getElementById('error_message')
+const StartingMinutes = 10;
+let time = StartingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+
+}
 
 
 form.addEventListener('submit', (e) => {
