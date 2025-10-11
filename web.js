8af8d56d53b9536b448cb4ae77b093a7d78f0fb7
chukwuisrael146-gif <chukwuisrael146@gmail.com>
2025-10-10 23:28:21 -0700

    My first commit

---
+++ web.js
@@ -0,0 +1,15 @@
+// let navbar = document.querySelector('nav');
+// let menulinks = document.getElementById("menulinks");
+
+// document.getElementById('menu-icon').addEventListener('click', function(){
+//     menulinks.classList.toggle('show-menu')
+// })
+
+// window.onscroll = function () {
+//     if (window.scrollY > 0) {
+//         navbar.style.background = 'red';
+//     }
+//     else {
+//         navbar.style.background = 'transparent';
+//     }
+// }
